f695ae91898f1157f6e08bb1ca872ba6f44b9db5
Jules Rivoire <julesriv@gmail.com>
2021-04-09 00:20:02 +0200

    [cub_parser] WIP passes simple tests and no map

--- parser/cub_parser.c
+++ parser/cub_parser.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 17:19:16 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/04/08 18:12:14 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/04/09 00:00:16 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,55 @@ static void init_description(t_des *to_fill)
 
 int cub_parser(int fd, t_des **to_fill)
 {
+	int	cnt_elems;
 	char *line;
 	char *identifiers = "RNOEASOWESFC";
+	char *type;
+	char *to_free;
 
 	*to_fill = malloc(sizeof(**to_fill));
 	if (!*to_fill)
 		return (1);
 	init_description(*to_fill);
-	// The first or first two char of a line are neccesarily it's identifier
+	cnt_elems = 0;
+	while (get_next_line(fd, &line) == 1 && cnt_elems < 8)
+	{
+		to_free = line;
+		type = ft_strchr(identifiers, line[0]);
+		if (!type && !ft_isspace(line[0]))
+		{
+			free(to_free);
+			return (1);
+		}
+		if (&identifiers[0] == type)
+		{
+			cnt_elems++;
+			if (resolution_parser(&line, *to_fill))
+			{
+				free(to_free);
+				return (1);
+			}
+		}
+		if (type < &identifiers[10] && type > &identifiers[0])
+		{
+			cnt_elems++;
+			if (texture_parser(&line, *to_fill))
+			{
+				free(to_free);
+				return (1);
+			}
+		}
+		if (type ==  &identifiers[10] || type == &identifiers[11])
+		{
+			cnt_elems++;
+			if (colour_parser(&line, *to_fill))
+			{
+				free(to_free);
+				return (1);
+			}
+		}
+		free(to_free);
+	}
+	free(line);
 	return (0);
 }
