ae53cf6f7076833b4eed82a31ad3c467c9ae7b09
Jules Rivoire <julesriv@gmail.com>
2021-04-08 17:43:50 +0200

    adding file structure for cub_parser

--- parser/cub_parser.c
+++ parser/cub_parser.c
@@ -1,31 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   cub_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 12:50:02 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/04/08 17:19:01 by jrivoire         ###   ########.fr       */
+/*   Created: 2021/04/08 17:19:16 by jrivoire          #+#    #+#             */
+/*   Updated: 2021/04/08 17:29:31 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _CUBD3D_H
-# define _CUBD3D_H
-# include "../libft/headers/libft.h"
-typedef struct s_description
-{
-	int x_res;
-	int y_res;
-	char *no_path;
-	char *ea_path;
-	char *so_path;
-	char *we_path;
-	char *s_path;
-	int floor_c;
-	int ceiling_c;
-	char **map;
-}	t_des;
+#include "parser.h"
 
-int	cub_parser(int fd, t_des *to_fill);
-#endif
+int cub_parser(int fd, t_des *to_fill)
+{
+	ft_putstr_fd("hi there", 1);
+	(void)fd;
+	(void)to_fill;
+	return (0);
+}
