a9de386a0accc28463150ae2a2c06b33e5daff52
Jules Rivoire <julesriv@gmail.com>
2021-04-08 11:14:06 +0200

    Add word count and parse_number functions to utils

--- headers/parser.h
+++ headers/parser.h
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 12:35:09 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/04/07 16:52:38 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/04/08 11:12:15 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 # include "cub3d.h"
 
 void	skip_space(char **line);
+int		count_words(char **words);
+int		parse_number(char *number);
+
 
 int		texture_parser(char **line, t_des *description);
 int		colour_parser(char **line, t_des *description);
--- parser/parser_utils.c
+++ parser/parser_utils.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 16:50:52 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/04/07 16:51:44 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/04/08 11:13:32 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,37 @@ void	skip_space(char **line)
 {
 	while (ft_isspace(**line))
 		(*line)++;
+}
+
+int	count_words(char **words)
+{
+	int count;
+
+	count = 0;
+	while(words[count])
+		count++;
+	return (count);
+}
+
+int	parse_number(char *number)
+{
+	int num;
+	int digit;
+
+	num = 0;
+	digit = 0;
+	while (number[num])
+	{
+		if (!ft_isspace(number[num]) && !ft_isdigit(number[num]))
+			return (-1);
+		if (ft_isdigit(number[num]))
+			digit = 1;
+		if (ft_isspace(number[num]) && digit == 1)
+			return (-1);
+		num++;
+	}
+	num = ft_atoi(number);
+	if (!digit)
+		return (-1);
+	return (num);
 }
