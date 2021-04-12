eac43acb555ae06fb4f8fcb945926242506808bc
Jules Rivoire <julesriv@gmail.com>
2021-04-12 10:19:32 +0200

    [cub_parser] full parser for simple cases

--- parser/cub_parser.c
+++ parser/cub_parser.c
@@ -6,7 +6,7 @@
 /*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 17:19:16 by jrivoire          #+#    #+#             */
-/*   Updated: 2021/04/12 10:07:31 by jrivoire         ###   ########.fr       */
+/*   Updated: 2021/04/12 10:48:47 by jrivoire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,18 @@ static void	init_description(t_des *to_fill)
 	to_fill->map = NULL;
 }
 
-static int	clean_fill(int result, int *cnt_elems)
+static int	clean_fill(int result, int *cnt_elems, char *to_free)
 {
 	if (result)
+	{
+		free(to_free);
 		return (1);
+	}
 	(*cnt_elems)++;
 	return (0);
 }
 
-static int	dispatch_line(char **line, t_des *to_fill, int *cnt_elems)
+static int	dispatch_line(char **line, t_des *to_fill, int *cnt_elems, char *to_free)
 {
 	char *identifiers;
 	char *type;
@@ -47,13 +50,13 @@ static int	dispatch_line(char **line, t_des *to_fill, int *cnt_elems)
 		return (1);
 	}
 	if (&identifiers[0] == type)
-		if (clean_fill(resolution_parser(line, to_fill), cnt_elems))
+		if (clean_fill(resolution_parser(line, to_fill), cnt_elems, to_free))
 			return (1);
 	if (type < &identifiers[10] && type > &identifiers[0])
-		if (clean_fill(texture_parser(line, to_fill), cnt_elems))
+		if (clean_fill(texture_parser(line, to_fill), cnt_elems, to_free))
 			return (1);
 	if (type == &identifiers[10] || type == &identifiers[11])
-		if (clean_fill(colour_parser(line, to_fill), cnt_elems))
+		if (clean_fill(colour_parser(line, to_fill), cnt_elems, to_free))
 			return (1);
 	return (0);
 }
@@ -74,13 +77,16 @@ int			cub_parser(int fd, t_des **to_fill)
 		to_free = line;
 		if (cnt_elems < 8)
 		{
-			if (dispatch_line(&line, *to_fill, &cnt_elems))
+			if (dispatch_line(&line, *to_fill, &cnt_elems, to_free))
 				return (1);
 		}
 		else
 		{
 			if (map_parser(&line, *to_fill))
+			{
+				free(to_free);
 				return (1);
+			}
 		}
 		free(to_free);
 	}
