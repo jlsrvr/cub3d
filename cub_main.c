a19fa094fdee20af4a7713e8d1866b9a70804958
Jules Rivoire <julesriv@gmail.com>
2021-04-19 12:47:58 +0200

    WIP Attempt at raycasting using lodev

--- cub_main.c
+++ cub_main.c
@@ -70,9 +70,41 @@
  return (ret);
  }*/
 
+/*char world_map[25][25] =
+{
+	"111111111111111111111111",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000010000000000001",
+	"100000000011111111000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000100000000000000001",
+	"100000010000000000000001",
+	"100000011000000000000001",
+	"100000011100000000000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"100000000000000000000001",
+	"111111111111111111111111",
+	NULL
+};*/
+
 #include <X11/X.h>
 #include <X11/keysym.h>
-
+#include <math.h>
+#define RED_PIXEL 0xFF0000
+#define WIN_X 1200
+#define WIN_Y 800
 
 typedef struct s_rect
 {
@@ -149,10 +181,6 @@ int handle_keyrelease(int keysym, void *data)
 	return (0);
 }
 
-#define RED_PIXEL 0xFF0000
-#define WIN_X 1200
-#define WIN_Y 800
-
 void	img_pix_put(t_img *img, int x, int y, int color)
 {
 	char    *pixel;
@@ -207,11 +235,51 @@ void	render_background(t_img *img, int color)
 
 int render(t_data *data)
 {
+	double pos_x = 5;
+	double pos_y = 6;
+	double dir_x = -1; // -1 west, 1 east
+	double dir_y = 0; // -1 south, 1 north
+	double plane_x = 0;
+	double plane_y = 0.66;
+	double time;
+	double old_time;
+	double camera_x;
+	double ray_dir_x;
+	double ray_dir_y;
+	int x = 0;
+	int w = WIN_X;
+	int map_x;
+	int map_y;
+	double side_dist_x;
+	double side_dist_y;
+	double  delta_dist_x;
+	double  delta_dist_y;
+	double perp_wall_dist;
+	int step_x;
+	int step_y;
+	int hit;
+	int side;
+
 	if (data->win_ptr == NULL)
 		return (1);
-	render_background(&data->img, 0xffffff);
-	render_rect(&data->img, (t_rect){WIN_X - 200, WIN_Y - 200, 100, 50, 0x00FF00});
-	render_rect(&data->img, (t_rect){0, WIN_Y - 150, 200, 50, 0x00FFFF});
+	render_rect(&data->img, (t_rect){0, WIN_Y * 0.5, WIN_X, WIN_Y / 2, 0x00FF00}); //floor
+	render_rect(&data->img, (t_rect){0, 0, WIN_X, WIN_Y / 2, 0x00FFFF}); //ceiling
+	while (x < w)
+	{
+		camera_x = 2 * x/ double(w) - 1;
+		ray_dir_x = dir_x + (plane_x * camera_x)
+		ray_dir_y= dir_y + (plane_y * camera_y)
+		map_x = (int)pos_x;
+		map_y = (int)pos_y;
+		delta_dist_x = 1;
+		if (!ray_dir_x)
+			delta_dist_x = fabs(1 / ray_dir_x);
+		delta_dist_y = 1;
+		if (!ray_dir_y)
+			delta_dist_y = fabs(1 / ray_dir_y);
+		hit = 0;
+		x++;
+	}
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 	return (0);
 }
@@ -221,7 +289,7 @@ int main(void)
 	t_data data;
 
 	data.mlx_ptr = mlx_init();
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WIN_X, WIN_Y, "I Have keys");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WIN_X, WIN_Y, "Raycasting simple");
 	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WIN_X, WIN_Y);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
