8eb0db15d5844c4219bec666bf242ac8dc00378a
Jules Rivoire <julesriv@gmail.com>
2021-04-20 09:40:20 +0200

    Rotating in the map but with glitch

--- cub_main.c
+++ cub_main.c
@@ -12,63 +12,6 @@
 
 #include "cub3d.h"
 
-/*static void	free_description(t_des *description)
-  {
-  int	index;
-
-  if (!description)
-  return ;
-  free(description->no_path);
-  free(description->ea_path);
-  free(description->so_path);
-  free(description->we_path);
-  free(description->s_path);
-  if (description->map)
-  {
-  index = -1;
-  while (description->map[++index])
-  free(description->map[index]);
-  free(description->map);
-  }
-  }
-
-  static int	pre_game_checks(char **av, int *fd, t_des *ptn_description)
-  {
-  if (path_checker(av[1], ".cub"))
-  {
-  printf(RED"Error\n"RESET"%s : invalid", av[1]);
-  return (1);
-  }
- *fd = open(av[1], O_RDONLY);
- if (cub_parser(*fd, &ptn_description))
- {
- printf(RED"Error\n"RESET);
- printf("There is a problem with the scene given at %s", av[1]);
- return (1);
- }
- else if (cub_validator(ptn_description))
- return (1);
- return (0);
- }
-
- int	main(int ac, char **av)
- {
- t_des	*ptn_description;
- int		ret;
- int		fd;
-
- ptn_description = NULL;
- ret = 0;
- if (ac == 1)
- {
- printf(RED"Error\n"RESET"A path to a scene description must be given!");
- return (1);
- }
- ret = pre_game_checks(av, &fd, ptn_description);
- close(fd);
- free_description(ptn_description);
- return (ret);
- }*/
 
 char world_map[10][10] =
 {
@@ -385,11 +328,11 @@ int handle_keypress(int keysym, t_data *data)
 		printf("Keypress 'a' (rotate left)\n");
 		double rot_speed = 3;
 		double old_dir_x = data->cast.dir_x;
-		data->cast.dir_x = data->cast.dir_x * cos(rot_speed) - data->cast.dir_y * sin(rot_speed);
-		data->cast.dir_y = old_dir_x * sin(-rot_speed) + data->cast.dir_y * cos(rot_speed);
+		data->cast.dir_x = data->cast.dir_x * cos(-rot_speed) - data->cast.dir_y * sin(-rot_speed);
+		data->cast.dir_y = old_dir_x * sin(-rot_speed) + data->cast.dir_y * cos(-rot_speed);
 		double old_plane_x = data->cast.plane_x;
-		data->cast.plane_x = data->cast.plane_x * cos(rot_speed) - data->cast.plane_y * sin(rot_speed);
-		data->cast.plane_y = old_plane_x * sin(-rot_speed) + data->cast.plane_y * cos(rot_speed);
+		data->cast.plane_x = data->cast.plane_x * cos(-rot_speed) - data->cast.plane_y * sin(-rot_speed);
+		data->cast.plane_y = old_plane_x * sin(-rot_speed) + data->cast.plane_y * cos(-rot_speed);
 		render_map(data);
 	}
 	else if (keysym == XK_s)
@@ -420,3 +363,61 @@ int main(void)
 	free(data.mlx_ptr);
 	return (0);
 }
+
+/*static void	free_description(t_des *description)
+  {
+  int	index;
+
+  if (!description)
+  return ;
+  free(description->no_path);
+  free(description->ea_path);
+  free(description->so_path);
+  free(description->we_path);
+  free(description->s_path);
+  if (description->map)
+  {
+  index = -1;
+  while (description->map[++index])
+  free(description->map[index]);
+  free(description->map);
+  }
+  }
+
+  static int	pre_game_checks(char **av, int *fd, t_des *ptn_description)
+  {
+  if (path_checker(av[1], ".cub"))
+  {
+  printf(RED"Error\n"RESET"%s : invalid", av[1]);
+  return (1);
+  }
+ *fd = open(av[1], O_RDONLY);
+ if (cub_parser(*fd, &ptn_description))
+ {
+ printf(RED"Error\n"RESET);
+ printf("There is a problem with the scene given at %s", av[1]);
+ return (1);
+ }
+ else if (cub_validator(ptn_description))
+ return (1);
+ return (0);
+ }
+
+ int	main(int ac, char **av)
+ {
+ t_des	*ptn_description;
+ int		ret;
+ int		fd;
+
+ ptn_description = NULL;
+ ret = 0;
+ if (ac == 1)
+ {
+ printf(RED"Error\n"RESET"A path to a scene description must be given!");
+ return (1);
+ }
+ ret = pre_game_checks(av, &fd, ptn_description);
+ close(fd);
+ free_description(ptn_description);
+ return (ret);
+ }*/
