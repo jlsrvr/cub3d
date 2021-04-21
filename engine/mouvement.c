e5111e993b9a0bd8b088781dd06709abc48ea5bf
Jules Rivoire <julesriv@gmail.com>
2021-04-21 11:43:03 +0200

    [engine_mouvement] Working and almost to norme v2

--- engine/mouvement.c
+++ engine/mouvement.c
@@ -1,10 +1,10 @@
 //////HEADER///
 //
 
-
 #include "engine.h"
 
-static void move_forward_backward(char dir, t_cast *cast, char **map, double move_speed)
+static void	move_forward_backward(char dir, t_cast *cast,
+									char **map, double move_speed)
 {
 	int inverser;
 	int next_x;
@@ -31,7 +31,8 @@ static void move_forward_backward(char dir, t_cast *cast, char **map, double mov
 	}
 }
 
-static void move_left_right(char dir, t_cast *cast, char **map, double move_speed)
+static void	move_left_right(char dir, t_cast *cast,
+								char **map, double move_speed)
 {
 	int inverser;
 	int next_x;
@@ -40,62 +41,61 @@ static void move_left_right(char dir, t_cast *cast, char **map, double move_spee
 	inverser = 1;
 	if (dir == 'R')
 		inverser *= -1;
-	next_x = (int)(cast->pos_x - (inverser * cast->dir_y * move_speed));
-	next_y = (int)(cast->pos_y + (inverser * cast->dir_x * move_speed));
+	next_x = (int)(cast->pos_x - (inverser * cast->plane_x * move_speed));
+	next_y = (int)(cast->pos_y - (inverser * cast->plane_y * move_speed));
 	if (map[next_x][(int)(cast->pos_y)] == '0')
 	{
-		if (dir == 'L')
-			cast->pos_x -= cast->dir_y * move_speed;
 		if (dir == 'R')
-			cast->pos_x += cast->dir_y * move_speed;
+			cast->pos_x += cast->plane_x * move_speed;
+		if (dir == 'L')
+			cast->pos_x -= cast->plane_x * move_speed;
 	}
 	if (map[(int)(cast->pos_x)][next_y] == '0')
 	{
-		if (dir == 'L')
-			cast->pos_y += cast->dir_x * move_speed;
 		if (dir == 'R')
-			cast->pos_y -= cast->dir_x * move_speed;
+			cast->pos_y += cast->plane_y * move_speed;
+		if (dir == 'L')
+			cast->pos_y -= cast->plane_y * move_speed;
 	}
 }
 
-static void rotate_player(char dir, t_cast *cast)
+static void	rotate_player(char dir, t_cast *cast)
 {
 	double old_dir_x;
 	double old_plane_x;
-	double rot_speed;
+	double speed;
 
 	old_dir_x = cast->dir_x;
 	old_plane_x = cast->plane_x;
-	rot_speed = 0.3;
+	speed = 0.3;
 	if (dir == 'R')
-		rot_speed *= -1;
-	cast->dir_x = cast->dir_x * cos(rot_speed) - cast->dir_y * sin(rot_speed);
-	cast->dir_y = old_dir_x * sin(rot_speed) + cast->dir_y * cos(rot_speed);
-	cast->plane_x = cast->plane_x * cos(rot_speed) - cast->plane_y * sin(rot_speed);
-	cast->plane_y = old_plane_x * sin(rot_speed) + cast->plane_y * cos(rot_speed);
+		speed *= -1;
+	cast->dir_x = cast->dir_x * cos(speed) - cast->dir_y * sin(speed);
+	cast->dir_y = old_dir_x * sin(speed) + cast->dir_y * cos(speed);
+	cast->plane_x = cast->plane_x * cos(speed) - cast->plane_y * sin(speed);
+	cast->plane_y = old_plane_x * sin(speed) + cast->plane_y * cos(speed);
 }
 
-int handle_keypress(int keysym, t_data *data)
+int			handle_keypress(int keysym, t_data *data)
 {
-	double move_speed = 0.5;
-	char **world_map = data->desc->map;
-	t_cast *cast = &data->cast;
+	double move_speed;
+	t_cast *cast;
 
-
+	cast = &data->cast;
+	move_speed = 0.5;
 	if (keysym == KEY_ESC)
 	{
-		// destroy image here??
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 		data->win_ptr = NULL;
 	}
 	else if (keysym == KEY_W)
-		move_forward_backward('F', cast, world_map, move_speed);
+		move_forward_backward('F', cast, data->desc->map, move_speed);
 	else if (keysym == KEY_S)
-		move_forward_backward('B', cast, world_map, move_speed);
+		move_forward_backward('B', cast, data->desc->map, move_speed);
 	else if (keysym == KEY_A)
-		move_left_right('L', cast, world_map, move_speed);
+		move_left_right('L', cast, data->desc->map, move_speed);
 	else if (keysym == KEY_D)
-		move_left_right('R', cast, world_map, move_speed);
+		move_left_right('R', cast, data->desc->map, move_speed);
 	else if (keysym == LEFT_ARROW)
 		rotate_player('L', cast);
 	else if (keysym == RIGHT_ARROW)
