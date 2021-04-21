55af0dcfab54976fe57cd95719e799509daaa8c1
Jules Rivoire <julesriv@gmail.com>
2021-04-21 10:34:02 +0200

    [engine_mouvement] assing cast to variable

--- engine/mouvement.c
+++ engine/mouvement.c
@@ -9,50 +9,54 @@ int handle_keypress(int keysym, t_data *data)
 	double rot_speed = 0.3;
 	double move_speed = 0.5;
 	char **world_map = data->desc->map;
+	t_cast *cast = &data->cast;
+
+
 	if (keysym == KEY_ESC)
 	{
+		// destroy image here??
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 		data->win_ptr = NULL;
 	}
 	else if (keysym == KEY_W)
 	{
 		printf("Keypress 'w' (forward)\n");
-		int next_x = (int)(data->cast.pos_x + data->cast.dir_x * move_speed);
-		int next_y = (int)(data->cast.pos_y + data->cast.dir_y * move_speed);
-		if (world_map[next_x][(int)(data->cast.pos_y)] == '0')
-			data->cast.pos_x += data->cast.dir_x * move_speed;
-		if (world_map[(int)(data->cast.pos_x)][next_y] == '0')
-			data->cast.pos_y += data->cast.dir_y * move_speed;
+		int next_x = (int)(cast->pos_x + cast->dir_x * move_speed);
+		int next_y = (int)(cast->pos_y + cast->dir_y * move_speed);
+		if (world_map[next_x][(int)(cast->pos_y)] == '0')
+			cast->pos_x += cast->dir_x * move_speed;
+		if (world_map[(int)(cast->pos_x)][next_y] == '0')
+			cast->pos_y += cast->dir_y * move_speed;
 	}
 	else if (keysym == KEY_S)
 	{
 		printf("Keypress 's' (backward)\n");
-		int next_x = (int)(data->cast.pos_x - data->cast.dir_x * move_speed);
-		int next_y = (int)(data->cast.pos_y - data->cast.dir_y * move_speed);
-		if (world_map[next_x][(int)(data->cast.pos_y)] == '0')
-			data->cast.pos_x -= data->cast.dir_x * move_speed;
-		if (world_map[(int)(data->cast.pos_x)][next_y] == '0')
-			data->cast.pos_y -= data->cast.dir_y * move_speed;
+		int next_x = (int)(cast->pos_x - cast->dir_x * move_speed);
+		int next_y = (int)(cast->pos_y - cast->dir_y * move_speed);
+		if (world_map[next_x][(int)(cast->pos_y)] == '0')
+			cast->pos_x -= cast->dir_x * move_speed;
+		if (world_map[(int)(cast->pos_x)][next_y] == '0')
+			cast->pos_y -= cast->dir_y * move_speed;
 	}
 	else if (keysym == KEY_A)
 	{
 		printf("Keypress 'a' (left)\n");
-		int next_x = (int)(data->cast.pos_x - data->cast.dir_y * move_speed);
-		int next_y = (int)(data->cast.pos_y + data->cast.dir_x * move_speed);
-		if (world_map[next_x][(int)(data->cast.pos_y)] == '0')
-			data->cast.pos_x -= data->cast.dir_y * move_speed;
-		if (world_map[(int)(data->cast.pos_x)][next_y] == '0')
-			data->cast.pos_y += data->cast.dir_x * move_speed;
+		int next_x = (int)(cast->pos_x - cast->dir_y * move_speed);
+		int next_y = (int)(cast->pos_y + cast->dir_x * move_speed);
+		if (world_map[next_x][(int)(cast->pos_y)] == '0')
+			cast->pos_x -= cast->dir_y * move_speed;
+		if (world_map[(int)(cast->pos_x)][next_y] == '0')
+			cast->pos_y += cast->dir_x * move_speed;
 	}
 	else if (keysym == KEY_D)
 	{
 		printf("Keypress 'd' (right)\n");
-		int next_x = (int)(data->cast.pos_x + data->cast.dir_y * move_speed);
-		int next_y = (int)(data->cast.pos_y - data->cast.dir_x * move_speed);
-		if (world_map[next_x][(int)(data->cast.pos_y)] == '0')
-			data->cast.pos_x += data->cast.dir_y * move_speed;
-		if (world_map[(int)(data->cast.pos_x)][next_y] == '0')
-			data->cast.pos_y -= data->cast.dir_x * move_speed;
+		int next_x = (int)(cast->pos_x + cast->dir_y * move_speed);
+		int next_y = (int)(cast->pos_y - cast->dir_x * move_speed);
+		if (world_map[next_x][(int)(cast->pos_y)] == '0')
+			cast->pos_x += cast->dir_y * move_speed;
+		if (world_map[(int)(cast->pos_x)][next_y] == '0')
+			cast->pos_y -= cast->dir_x * move_speed;
 	}
 	else if (keysym == LEFT_ARROW)
 	{
