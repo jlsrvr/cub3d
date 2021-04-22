8eea2827f0e982e5c00d2551ac7970efc0122500
Jules Rivoire <julesriv@gmail.com>
2021-04-22 21:39:02 +0200

    [cub_engine] refactoring not normed

--- engine/cub_engine.c
+++ engine/cub_engine.c
@@ -53,52 +53,84 @@ static int	render_view(t_data *data)
 
 static void	destroy_mlx_image(void **ptr_to_ptr, void *mlx_ptr)
 {
-	if(!(*ptr_to_ptr))
+	if (!(*ptr_to_ptr))
 		return ;
 	mlx_destroy_image(mlx_ptr, *ptr_to_ptr);
 }
 
 static int	destroy_mlx_resources(t_data *data)
 {
-		destroy_mlx_image((void **)&data->img.mlx_img, data->mlx_ptr);
-		destroy_mlx_image((void **)&data->textures[0].img.mlx_img, data->mlx_ptr);
-		destroy_mlx_image((void **)&data->textures[1].img.mlx_img, data->mlx_ptr);
-		destroy_mlx_image((void **)&data->textures[2].img.mlx_img, data->mlx_ptr);
-		destroy_mlx_image((void **)&data->textures[3].img.mlx_img, data->mlx_ptr);
-		if (data->win_ptr)
-			mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-		mlx_destroy_display(data->mlx_ptr);
-		free(data->mlx_ptr);
-		return (1);
+	destroy_mlx_image((void **)&data->img.mlx_img, data->mlx_ptr);
+	destroy_mlx_image((void **)&data->textures[0].img.mlx_img, data->mlx_ptr);
+	destroy_mlx_image((void **)&data->textures[1].img.mlx_img, data->mlx_ptr);
+	destroy_mlx_image((void **)&data->textures[2].img.mlx_img, data->mlx_ptr);
+	destroy_mlx_image((void **)&data->textures[3].img.mlx_img, data->mlx_ptr);
+	if (data->win_ptr)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	return (1);
+}
+
+static void	open_textures(t_data *data)
+{
+	t_texture	*texture;
+	int			index;
+	char		*paths[4];
+
+	paths[0] = data->desc->no_path;
+	paths[1] = data->desc->ea_path;
+	paths[2] = data->desc->so_path;
+	paths[3] = data->desc->we_path;
+	index = -1;
+	while (++index < 4)
+	{
+		texture = &data->textures[index];
+		texture->img.mlx_img = mlx_xpm_file_to_image(data->mlx_ptr,
+					paths[index], &texture->width, &texture->height);
+	}
+}
+
+static int	init_images(t_data *data)
+{
+	int			index;
+	t_texture	*texture;
+
+	open_textures(data);
+	data->img.mlx_img = mlx_new_image(data->mlx_ptr, data->desc->x_res,
+										data->desc->y_res);
+	if (!(data->img.mlx_img) || !(data->textures[0].img.mlx_img)
+			|| !(data->textures[1].img.mlx_img)
+			|| !(data->textures[2].img.mlx_img)
+			|| !(data->textures[3].img.mlx_img))
+		return (destroy_mlx_resources(data));
+	index = -1;
+	while (++index < 4)
+	{
+		texture = &data->textures[index];
+		texture->addr = (int *)mlx_get_data_addr(texture->img.mlx_img,
+								&texture->img.bpp, &texture->img.line_len,
+								&texture->img.endian);
+	}
+	data->img.addr = mlx_get_data_addr(data->img.mlx_img,
+						&data->img.bpp, &data->img.line_len, &data->img.endian);
+	return (0);
 }
 
 int			cub_engine(t_des *description)
 {
 	t_data	data;
-	int		win_x;
-	int		win_y;
 
-	win_x = description->x_res;
-	win_y = description->y_res;
 	data.desc = description;
 	data.mlx_ptr = mlx_init();
 	if (!(data.mlx_ptr))
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, win_x, win_y, "Jules' Cub3D");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, data.desc->x_res,
+									data.desc->y_res, "Jules' Cub3D");
 	if (!(data.win_ptr))
 		return (destroy_mlx_resources(&data));
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, win_x, win_y);
-	data.textures[0].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->no_path, &data.textures[0].width, &data.textures[0].height);
-	data.textures[1].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->ea_path, &data.textures[1].width, &data.textures[1].height);
-	data.textures[2].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->so_path, &data.textures[2].width, &data.textures[2].height);
-	data.textures[3].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->we_path, &data.textures[3].width, &data.textures[3].height);
-	if (!(data.img.mlx_img) || !(data.textures[0].img.mlx_img) || !(data.textures[1].img.mlx_img)|| !(data.textures[2].img.mlx_img)|| !(data.textures[3].img.mlx_img))
-		return (destroy_mlx_resources(&data));
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
-	data.textures[0].addr = (int *)mlx_get_data_addr(data.textures[0].img.mlx_img, &data.textures[0].img.bpp, &data.textures[0].img.line_len, &data.textures[0].img.endian);
-	data.textures[1].addr = (int *)mlx_get_data_addr(data.textures[1].img.mlx_img, &data.textures[1].img.bpp, &data.textures[1].img.line_len, &data.textures[1].img.endian);
-	data.textures[2].addr = (int *)mlx_get_data_addr(data.textures[2].img.mlx_img, &data.textures[2].img.bpp, &data.textures[2].img.line_len, &data.textures[2].img.endian);
-	data.textures[3].addr = (int *)mlx_get_data_addr(data.textures[3].img.mlx_img, &data.textures[3].img.bpp, &data.textures[3].img.line_len, &data.textures[3].img.endian);
+	if (init_images(&data))
+		return (1);
 	init_raycaster(&data.cast, description);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_loop_hook(data.mlx_ptr, &render_view, &data);
