df42a7ca9efa9bc89e1d1c4dda703e042e4532e0
Jules Rivoire <julesriv@gmail.com>
2021-04-22 22:21:56 +0200

    [engine.h] normed v2

--- headers/engine.h
+++ headers/engine.h
@@ -24,98 +24,98 @@
 # define LEFT_ARROW 0xff51
 # define RIGHT_ARROW 0xff53
 
-typedef struct 	s_transfer
+typedef struct	s_transfer
 {
 	double	wall_x;
 	double	step;
 	double	tex_pos;
 	int		tex_x;
 
-} 				t_transfer;
+}				t_transfer;
 
-typedef struct s_ray
+typedef struct	s_ray
 {
-        int x;
-        int y_start;
-        int y_end;
-}       t_ray;
+	int x;
+	int y_start;
+	int y_end;
+}				t_ray;
 
-typedef struct s_cast
+typedef struct	s_cast
 {
-        int width;
-        int height;
-        double pos_x;
-        double pos_y;
-        double dir_x;
-        double dir_y;
-        double plane_x;
-        double plane_y;
-        double camera_x;
-        double ray_dir_x;
-        double ray_dir_y;
-        int map_x;
-        int map_y;
-        double side_dist_x;
-        double side_dist_y;
-        double  delta_dist_x;
-        double  delta_dist_y;
-        double perp_wall_dist;
-        int step_x;
-        int step_y;
-        int hit;
-        int side;
-        int line_height;
-        int draw_start;
-        int draw_end;
-        int colour;
-}	t_cast;
+	int		width;
+	int		height;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	int		map_x;
+	int		map_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	perp_wall_dist;
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+	int		colour;
+}				t_cast;
 
-typedef struct s_rect
+typedef struct	s_rect
 {
-        int     x;
-        int     y;
-        int width;
-        int height;
-        int color;
+	int	x;
+	int	y;
+	int	width;
+	int	height;
+	int	color;
 
-}       t_rect;
+}				t_rect;
 
-typedef struct s_img
+typedef struct	s_img
 {
-        void    *mlx_img;
-        char    *addr;
-        int             bpp;
-        int             line_len;
-        int             endian;
-}       t_img;
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}				t_img;
 
-typedef struct s_texture
+typedef struct	s_texture
 {
-	t_img img;
-	int *addr;
-	int width;
-	int height;
-} t_texture;
+	t_img	img;
+	int		*addr;
+	int		width;
+	int		height;
+}				t_texture;
 
-typedef struct s_data
+typedef struct	s_data
 {
-        void    *mlx_ptr;
-        void    *win_ptr;
-        t_cast  cast;
-        t_img   img;
-        t_des 	*desc;
-		t_texture textures[5];
+	void		*mlx_ptr;
+	void		*win_ptr;
+	t_cast		cast;
+	t_img		img;
+	t_des		*desc;
+	t_texture	textures[5];
 
-}       t_data;
+}				t_data;
 
-int		destroy_mlx_resources(t_data *data);
-int		handle_keypress(int keysym, t_data *data);
-void	calculate_distance_to_wall(t_cast *cast);
-void	cast_ray(t_cast *cast, t_des *desc);
-void	define_line_start_end(t_cast *cast);
-void	img_pix_put(t_img *img, int x, int y, int color);
-void	init_raycaster(t_cast *cast, t_des *desc);
-void	init_raycaster_loop(t_cast *cast, int x, int w);
-void 	render_ray(t_data *data, t_ray ray);
-void	render_rect(t_img *img, t_rect rect);
+int				destroy_mlx_resources(t_data *data);
+int				handle_keypress(int keysym, t_data *data);
+void			calculate_distance_to_wall(t_cast *cast);
+void			cast_ray(t_cast *cast, t_des *desc);
+void			define_line_start_end(t_cast *cast);
+void			img_pix_put(t_img *img, int x, int y, int color);
+void			init_raycaster(t_cast *cast, t_des *desc);
+void			init_raycaster_loop(t_cast *cast, int x, int w);
+void			render_ray(t_data *data, t_ray ray);
+void			render_rect(t_img *img, t_rect rect);
 #endif
