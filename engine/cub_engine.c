2f7e1041f9798ad0590079bedefe0585909bb749
Jules Rivoire <julesriv@gmail.com>
2021-04-22 15:51:49 +0200

    [cub_engine]beginning normed v2

--- engine/cub_engine.c
+++ engine/cub_engine.c
@@ -1,14 +1,23 @@
-//////HEADER?????//
-//
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub_engine.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrivoire <jrivoire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/22 15:31:48 by jrivoire          #+#    #+#             */
+/*   Updated: 2021/04/22 15:42:45 by jrivoire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "engine.h"
 
-static int raycaster(t_data *data)
+static int	raycaster(t_data *data)
 {
-	t_cast *cast;
-	int x = 0;
+	t_cast	*cast;
+	int		x;
 
+	x = 0;
 	cast = &data->cast;
 	while (x < cast->width)
 	{
@@ -23,7 +32,7 @@ static int raycaster(t_data *data)
 	return (0);
 }
 
-static int render_view(t_data *data)
+static int	render_view(t_data *data)
 {
 	int win_x;
 	int win_y;
@@ -32,34 +41,50 @@ static int render_view(t_data *data)
 		return (1);
 	win_x = data->desc->x_res;
 	win_y = data->desc->y_res;
-	render_rect(&data->img, (t_rect){0, win_y * 0.5, win_x, win_y / 2, data->desc->floor_c}); //data->description.res
-	render_rect(&data->img, (t_rect){0, 0, win_x, win_y / 2, data->desc->ceiling_c}); //data->description.res
+	render_rect(&data->img,
+			(t_rect){0, win_y * 0.5, win_x, win_y / 2, data->desc->floor_c});
+	render_rect(&data->img,
+			(t_rect){0, 0, win_x, win_y / 2, data->desc->ceiling_c});
 	raycaster(data);
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
+	mlx_put_image_to_window(data->mlx_ptr,
+			data->win_ptr, data->img.mlx_img, 0, 0);
 	return (0);
 }
 
-int cub_engine(t_des *description)
+int			cub_engine(t_des *description)
 {
-	t_data data;
-	int win_x;
-	int win_y;
+	t_data	data;
+	int		win_x;
+	int		win_y;
 
 	win_x = description->x_res;
 	win_y = description->y_res;
 	data.desc = description;
-	data.mlx_ptr = mlx_init(); //protect this form being NULL
-	data.win_ptr = mlx_new_window(data.mlx_ptr, win_x, win_y, "Jules' Cub3D");//protect this form being NULL
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, win_x, win_y);//protect this form being NULL
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);//protect this form being NULL
-	data.textures[0].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->no_path, &data.textures[0].width, &data.textures[0].height);//protect this form being NULL
-	data.textures[0].addr = (int *)mlx_get_data_addr(data.textures[0].img.mlx_img, &data.textures[0].img.bpp, &data.textures[0].img.line_len, &data.textures[0].img.endian);//protect this form being NULL
-	data.textures[1].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->ea_path, &data.textures[1].width, &data.textures[1].height);//protect this form being NULL
-	data.textures[1].addr = (int *)mlx_get_data_addr(data.textures[1].img.mlx_img, &data.textures[1].img.bpp, &data.textures[1].img.line_len, &data.textures[1].img.endian);//protect this form being NULL
-	data.textures[2].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->so_path, &data.textures[2].width, &data.textures[2].height);//protect this form being NULL
-	data.textures[2].addr = (int *)mlx_get_data_addr(data.textures[2].img.mlx_img, &data.textures[2].img.bpp, &data.textures[2].img.line_len, &data.textures[2].img.endian);//protect this form being NULL
-	data.textures[3].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->we_path, &data.textures[3].width, &data.textures[3].height);//protect this form being NULL
-	data.textures[3].addr = (int *)mlx_get_data_addr(data.textures[3].img.mlx_img, &data.textures[3].img.bpp, &data.textures[3].img.line_len, &data.textures[3].img.endian);//protect this form being NULL
+	//protect this form being NULL
+	data.mlx_ptr = mlx_init();
+	//protect this form being NULL
+	data.win_ptr = mlx_new_window(data.mlx_ptr, win_x, win_y, "Jules' Cub3D");
+	//protect this form being NULL
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, win_x, win_y);
+	//protect this form being NULL
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
+	//protect this form being NULL
+	data.textures[0].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->no_path, &data.textures[0].width, &data.textures[0].height);
+	//protect this form being NULL
+	data.textures[0].addr = (int *)mlx_get_data_addr(data.textures[0].img.mlx_img, &data.textures[0].img.bpp, &data.textures[0].img.line_len, &data.textures[0].img.endian);
+	//protect this form being NULL
+	data.textures[1].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->ea_path, &data.textures[1].width, &data.textures[1].height);
+	//protect this form being NULL
+	data.textures[1].addr = (int *)mlx_get_data_addr(data.textures[1].img.mlx_img, &data.textures[1].img.bpp, &data.textures[1].img.line_len, &data.textures[1].img.endian);
+	//protect this form being NULL
+	data.textures[2].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->so_path, &data.textures[2].width, &data.textures[2].height);
+	//protect this form being NULL
+	data.textures[2].addr = (int *)mlx_get_data_addr(data.textures[2].img.mlx_img, &data.textures[2].img.bpp, &data.textures[2].img.line_len, &data.textures[2].img.endian);
+	//protect this form being NULL
+	data.textures[3].img.mlx_img = mlx_xpm_file_to_image(data.mlx_ptr, data.desc->we_path, &data.textures[3].width, &data.textures[3].height);
+	//protect this form being NULL
+	data.textures[3].addr = (int *)mlx_get_data_addr(data.textures[3].img.mlx_img, &data.textures[3].img.bpp, &data.textures[3].img.line_len, &data.textures[3].img.endian);
+	//protect this form being NULL
 	init_raycaster(&data.cast, description);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_loop_hook(data.mlx_ptr, &render_view, &data);
