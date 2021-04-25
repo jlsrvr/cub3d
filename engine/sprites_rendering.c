ea5063c3b04eead5b7cbf955b20e1dd99d248d2b
Jules Rivoire <julesriv@gmail.com>
2021-04-25 13:53:26 +0200

    [add_sprites] use new struct and move init of order

--- engine/sprites_rendering.c
+++ engine/sprites_rendering.c
@@ -24,7 +24,6 @@ static void sort_sprites(int *order, double *dist, int amount)
 	{
 		icomp = index;
 		while (++icomp < amount)
-		{
 			if (dist[index] < dist[icomp])
 			{
 				temp_order = order[index];
@@ -34,38 +33,40 @@ static void sort_sprites(int *order, double *dist, int amount)
 				order[icomp] = temp_order;
 				dist[icomp] = temp_dist;
 			}
-		}
 	}
 }
 
-int	add_sprites(t_data *data)
+typedef struct	s_draw_sprite_vars
+{
+	double	sprite_x;
+	double	sprite_y;
+	double	inv_det;
+	double	transform_x;
+	double	transform_y;
+	int		sprite_screen_x;
+	int		sprite_height;
+	int		sprite_width;
+	int		draw_start_y;
+	int		draw_end_y;
+	int		draw_start_x;
+	int		draw_end_x;
+	int		stripe;
+	int		tex_x;
+	int		tex_y;
+	int		y;
+	int		d;
+	int		colour;
+
+}				t_draw_sprite_vars;
+
+int init_sprite_order(t_data *data, int **sprite_order, double **sprite_dist)
 {
 	int index;
-	int	*sprite_order;
-	double *sprite_dist;
 	t_sprite sprite;
-	double sprite_x;
-	double sprite_y;
-	double inv_det;
-	double transform_x;
-	double transform_y;
-	int sprite_screen_x;
-	int sprite_height;
-	int sprite_width;
-	int draw_start_y;
-	int draw_end_y;
-	int draw_start_x;
-	int draw_end_x;
-	int stripe;
-	int tex_x;
-	int tex_y;
-	int y;
-	int d;
-	int colour;
 
-	sprite_order = malloc(sizeof(*sprite_order) * data->desc->sprite_cnt);
-	sprite_dist = malloc(sizeof(*sprite_dist) * data->desc->sprite_cnt);
-	if (!sprite_order || !sprite_dist)
+	*sprite_order = malloc(sizeof(**sprite_order) * data->desc->sprite_cnt);
+	*sprite_dist = malloc(sizeof(**sprite_dist) * data->desc->sprite_cnt);
+	if (!*sprite_order || !*sprite_dist)
 	{
 		free(sprite_order);
 		free(sprite_dist);
@@ -75,48 +76,59 @@ int	add_sprites(t_data *data)
 	while (++index < data->desc->sprite_cnt)
 	{
 		sprite = data->desc->sprites[index];
-		sprite_order[index] = index;
-		sprite_dist[index] = ((data->cast.pos_x - sprite.pos_x) * (data->cast.pos_x - sprite.pos_x) + (data->cast.pos_y - sprite.pos_y) * (data->cast.pos_y - sprite.pos_y));
+		(*sprite_order)[index] = index;
+		(*sprite_dist)[index] = ((data->cast.pos_x - sprite.pos_x) * (data->cast.pos_x - sprite.pos_x) + (data->cast.pos_y - sprite.pos_y) * (data->cast.pos_y - sprite.pos_y));
 	}
-	sort_sprites(sprite_order, sprite_dist, data->desc->sprite_cnt);
+	sort_sprites(*sprite_order, *sprite_dist, data->desc->sprite_cnt);
+	return (0);
+}
 
+int	add_sprites(t_data *data)
+{
+	int index;
+	int	*sprite_order;
+	double *sprite_dist;
+	t_draw_sprite_vars draw_vars;
+
+	if (init_sprite_order(data, &sprite_order, &sprite_dist))
+		return (1);
 	index = -1;
 	while (++index < data->desc->sprite_cnt)
 	{
-		sprite_x = data->desc->sprites[sprite_order[index]].pos_x - data->cast.pos_x;
-		sprite_y = data->desc->sprites[sprite_order[index]].pos_y - data->cast.pos_y;
-		inv_det = 1.0 / (data->cast.plane_x * data->cast.dir_y - data->cast.dir_x * data->cast.plane_y);
-		transform_x = inv_det * (data->cast.dir_y * sprite_x - data->cast.dir_x * sprite_y);
-		transform_y = inv_det * (-data->cast.plane_y * sprite_x + data->cast.plane_x * sprite_y);
-		sprite_screen_x = (int)((data->cast.width / 2) * (1 + transform_x / transform_y));
-		sprite_height = abs((int)(data->cast.height / (transform_y)));
-		draw_start_y = -sprite_height / 2 + data->cast.height / 2;
-		if (draw_start_y < 0)
-			draw_start_y = 0;
-		draw_end_y = sprite_height / 2 + data->cast.height / 2;
-		if (draw_end_y >= data->cast.height)
-			draw_end_y = data->cast.height - 1;
-		sprite_width = abs((int)(data->cast.height / (transform_y))); //seems weird to use height again here
-		draw_start_x = -sprite_width / 2 + sprite_screen_x;
-		if (draw_start_x < 0)
-			draw_start_x = 0;
-		draw_end_x = sprite_width / 2 + sprite_screen_x;
-		if (draw_end_x >= data->cast.width)
-			draw_end_x = data->cast.width - 1;
-		stripe = draw_start_x - 1;
-		while (++stripe < draw_end_x)
+		draw_vars.sprite_x = data->desc->sprites[sprite_order[index]].pos_x - data->cast.pos_x;
+		draw_vars.sprite_y = data->desc->sprites[sprite_order[index]].pos_y - data->cast.pos_y;
+		draw_vars.inv_det = 1.0 / (data->cast.plane_x * data->cast.dir_y - data->cast.dir_x * data->cast.plane_y);
+		draw_vars.transform_x = draw_vars.inv_det * (data->cast.dir_y * draw_vars.sprite_x - data->cast.dir_x * draw_vars.sprite_y);
+		draw_vars.transform_y = draw_vars.inv_det * (-data->cast.plane_y * draw_vars.sprite_x + data->cast.plane_x * draw_vars.sprite_y);
+		draw_vars.sprite_screen_x = (int)((data->cast.width / 2) * (1 + draw_vars.transform_x / draw_vars.transform_y));
+		draw_vars.sprite_height = abs((int)(data->cast.height / (draw_vars.transform_y)));
+		draw_vars.draw_start_y = -draw_vars.sprite_height / 2 + data->cast.height / 2;
+		if (draw_vars.draw_start_y < 0)
+			draw_vars.draw_start_y = 0;
+		draw_vars.draw_end_y = draw_vars.sprite_height / 2 + data->cast.height / 2;
+		if (draw_vars.draw_end_y >= data->cast.height)
+			draw_vars.draw_end_y = data->cast.height - 1;
+		draw_vars.sprite_width = abs((int)(data->cast.height / (draw_vars.transform_y)));
+		draw_vars.draw_start_x = -draw_vars.sprite_width / 2 + draw_vars.sprite_screen_x;
+		if (draw_vars.draw_start_x < 0)
+			draw_vars.draw_start_x = 0;
+		draw_vars.draw_end_x = draw_vars.sprite_width / 2 + draw_vars.sprite_screen_x;
+		if (draw_vars.draw_end_x >= data->cast.width)
+			draw_vars.draw_end_x = data->cast.width - 1;
+		draw_vars.stripe = draw_vars.draw_start_x - 1;
+		while (++draw_vars.stripe < draw_vars.draw_end_x)
 		{
-			tex_x = (int)(256 * (stripe - (-sprite_width / 2 + sprite_screen_x)) * data->textures[4].width / sprite_width) / 256;
-			if (transform_y > 0 && stripe > 0 && stripe < data->cast.width && transform_y < data->wall_dist[stripe])
+			draw_vars.tex_x = (int)(256 * (draw_vars.stripe - (-draw_vars.sprite_width / 2 + draw_vars.sprite_screen_x)) * data->textures[4].width / draw_vars.sprite_width) / 256;
+			if (draw_vars.transform_y > 0 && draw_vars.stripe > 0 && draw_vars.stripe < data->cast.width && draw_vars.transform_y < data->wall_dist[draw_vars.stripe])
 			{
-				y = draw_start_y - 1;
-				while (++y < draw_end_y)
+				draw_vars.y = draw_vars.draw_start_y - 1;
+				while (++draw_vars.y < draw_vars.draw_end_y)
 				{
-					d = (y) * 256 - data->cast.height *128 + sprite_height * 128;
-					tex_y = ((d * data->textures[4].height) / sprite_height) / 256;
-					colour = data->textures[4].addr[data->textures[4].width * tex_y + tex_x];
-					if ((colour & 0x00FFFFFF) != 0)
-						img_pix_put(&data->img, stripe, y, colour);
+					draw_vars.d = (draw_vars.y) * 256 - data->cast.height *128 + draw_vars.sprite_height * 128;
+					draw_vars.tex_y = ((draw_vars.d * data->textures[4].height) / draw_vars.sprite_height) / 256;
+					draw_vars.colour = data->textures[4].addr[data->textures[4].width * draw_vars.tex_y + draw_vars.tex_x];
+					if ((draw_vars.colour & 0x00FFFFFF) != 0)
+						img_pix_put(&data->img, draw_vars.stripe, draw_vars.y, draw_vars.colour);
 				}
 			}
 		}
