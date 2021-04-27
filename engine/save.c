4a7834d5c6d00ddb48b42283c93bac8bf4e1dacb
Jules Rivoire <julesriv@gmail.com>
2021-04-27 19:52:19 +0200

    WIP save mostly works expect for big image

--- engine/save.c
+++ engine/save.c
@@ -11,29 +11,29 @@ int save_image(t_data *data)
 	int fd;
 
 	data->win_ptr = NULL;
-	fd = open("./screenshot.bmp", O_WRONLY| O_CREAT);
+	fd = open("./screenshot.bmp", O_WRONLY| O_CREAT | O_TRUNC);
 	if (fd == -1)
 	{
 		printf(RED"Error\n"RESET"Unable to create or open file for screenshot");
 		return (1);
 	}
 	//*****HEADER************//
-	int bytes_per_pixel = data->img.bpp / 8;
-	char *sign = "BM";
+	int bytes_per_pixel;
 	int height;
 	int width;
 
+	bytes_per_pixel = data->img.bpp / 8;
 	height = data->desc->y_res;
-	width = data->desc->y_res;
-	write(fd, sign, 2);
+	width = data->desc->x_res;
+	write(fd, "BM", 2);
 	int padded_row_size = (int)(4 * ceil((float)width/4.0f))*bytes_per_pixel;
 	int file_size = padded_row_size*height + HEADER_SIZE + INFO_HEADER_SIZE;
 	write(fd, &file_size, 4);
 	int reserved = 0x0000;
 	write(fd, &reserved, 4);
-	int dataOffset = HEADER_SIZE+INFO_HEADER_SIZE;
-	//*******INFO*HEADER******//
+	int dataOffset = HEADER_SIZE + INFO_HEADER_SIZE;
 	write(fd, &dataOffset, 4);
+	//*******INFO*HEADER******//
 	int infoHeaderSize = INFO_HEADER_SIZE;
 	write(fd, &infoHeaderSize, 4);
 	write(fd, &width, 4);
@@ -47,8 +47,8 @@ int save_image(t_data *data)
 	//write image size (in bytes)
 	int imageSize = height*width*bytes_per_pixel;
 	write(fd, &imageSize, 4);
-	int resolution_x = 11811; //300 dpi
-	int resolution_y = 11811; //300 dpi
+	int resolution_x = 0; //300 dpi
+	int resolution_y = 0; //300 dpi
 	write(fd, &resolution_x, 4);
 	write(fd, &resolution_y, 4);
 	int colors_used = MAX_NUMBER_OF_COLOURS;
@@ -56,11 +56,11 @@ int save_image(t_data *data)
 	int important_colors = ALL_COLORS_REQUIRED;
 	write(fd, &important_colors, 4);
 	int i = 0;
-	int unpadded_row_size = width*bytes_per_pixel;
+	int unpadded_row_size = width * bytes_per_pixel;
 	for ( i = 0; i < height; i++)
 	{
-		int pixel_offset = ((height- i) - 1)*unpadded_row_size;
-		write(fd, &data->img.addr[pixel_offset], 1);
+		int pixel_offset = ((height- i) - 1) * unpadded_row_size;
+		write(fd, &data->img.addr[pixel_offset], padded_row_size);
 	}
 	close(fd);
 	destroy_mlx_resources(data);
