0d8a49d4f27f9961f296212303eb6117af4d91d1
Jules Rivoire <julesriv@gmail.com>
2021-04-28 11:57:29 +0200

    [save] begin refacto

--- engine/save.c
+++ engine/save.c
@@ -2,13 +2,67 @@
 
 #define HEADER_SIZE 14
 #define INFO_HEADER_SIZE 40
-#define NO_COMPRESSION 0
-#define MAX_NUMBER_OF_COLOURS 0
-#define ALL_COLORS_REQUIRED 0
 
-int save_image(t_data *data)
+static void	compression_and_colour(int fd)
+{
+	int temp;
+
+	temp = 0;
+	write(fd, &temp, 4);
+	write(fd, &temp, 4);
+	write(fd, &temp, 4);
+	write(fd, &temp, 4);
+	write(fd, &temp, 4);
+	write(fd, &temp, 4);
+}
+
+static void	info_header(int fd, t_data *data, int width, int height)
+{
+	int temp;
+	int plane;
+	int reserved;
+	int data_offset;
+
+	reserved = 0x0000;
+	write(fd, &reserved, 4);
+	data_offset = HEADER_SIZE + INFO_HEADER_SIZE;
+	write(fd, &data_offset, 4);
+	temp = INFO_HEADER_SIZE;
+	write(fd, &temp, 4);
+	write(fd, &width, 4);
+	write(fd, &height, 4);
+	plane = 1;
+	write(fd, &plane, 2);
+	write(fd, &data->img.bpp, 2);
+	compression_and_colour(fd);
+}
+
+static void	write_in_file(int fd, t_data *data, int padded_row_size)
+{
+	int i;
+	int height;
+	int width;
+	int pixel_offset;
+	int unpadded_row_size;
+
+	height = data->desc->y_res;
+	width = data->desc->x_res;
+	unpadded_row_size = width * (data->img.bpp / 8);
+	i = -1;
+	while (++i < height)
+	{
+		pixel_offset = ((height - i) - 1) * unpadded_row_size;
+		write(fd, &data->img.addr[pixel_offset], padded_row_size);
+	}
+}
+
+int	save_image(t_data *data)
 {
 	int fd;
+	int height;
+	int width;
+	int temp;
+	int padded_row_size;
 
 	data->win_ptr = NULL;
 	fd = open("./screenshot.bmp", O_WRONLY | O_TRUNC | O_CREAT, 0755);
@@ -17,51 +71,15 @@ int save_image(t_data *data)
 		printf(RED"Error\n"RESET"Unable to create or open file for screenshot");
 		return (1);
 	}
-	//*****HEADER************//
-	int bytes_per_pixel;
-	int height;
-	int width;
-
-	bytes_per_pixel = data->img.bpp / 8;
 	height = data->desc->y_res;
 	width = data->desc->x_res;
 	write(fd, "BM", 2);
-	int padded_row_size = (int)(4 * ceil((float)width / 4.0f)) * bytes_per_pixel;
-	int file_size = padded_row_size * height + HEADER_SIZE + INFO_HEADER_SIZE;
-	write(fd, &file_size, 4);
-	int reserved = 0x0000;
-	write(fd, &reserved, 4);
-	int dataOffset = HEADER_SIZE + INFO_HEADER_SIZE;
-	write(fd, &dataOffset, 4);
-	//*******INFO*HEADER******//
-	int infoHeaderSize = INFO_HEADER_SIZE;
-	write(fd, &infoHeaderSize, 4);
-	write(fd, &width, 4);
-	write(fd, &height, 4);
-	int planes = 1; //always 1
-	write(fd, &planes, 2);
-	write(fd, &data->img.bpp, 2);
-	//write compression
-	int compression = NO_COMPRESSION;
-	write(fd, &compression, 4);
-	//write image size (in bytes)
-	int imageSize = height * width * bytes_per_pixel;
-	write(fd, &imageSize, 4);
-	int resolution_x = 0; //300 dpi
-	int resolution_y = 0; //300 dpi
-	write(fd, &resolution_x, 4);
-	write(fd, &resolution_y, 4);
-	int colors_used = MAX_NUMBER_OF_COLOURS;
-	write(fd, &colors_used, 4);
-	int important_colors = ALL_COLORS_REQUIRED;
-	write(fd, &important_colors, 4);
-	int i = 0;
-	int unpadded_row_size = width * bytes_per_pixel;
-	for ( i = 0; i < height; i++)
-	{
-		int pixel_offset = ((height- i) - 1) * unpadded_row_size;
-		write(fd, &data->img.addr[pixel_offset], padded_row_size);
-	}
+	padded_row_size = (int)(4 * ceil((float)width / 4.0f))
+							* (data->img.bpp / 8);
+	temp = padded_row_size * height + HEADER_SIZE + INFO_HEADER_SIZE;
+	write(fd, &temp, 4);
+	info_header(fd, data, width, height);
+	write_in_file(fd, data, padded_row_size);
 	close(fd);
 	destroy_mlx_resources(data);
 	return (0);
